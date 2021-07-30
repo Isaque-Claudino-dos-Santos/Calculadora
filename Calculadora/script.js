bf1ebf2d80ec365f8f5e1f80c4e6ed49e6b538d1
Isaque Claudino <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2021-07-30 15:45:58 -0300

    Add files via upload
    design front-end

---
+++ Calculadora/script.js
@@ -0,0 +1,23 @@
+
+
+function pegarValor(valor) {
+    exibir(valor);
+}
+
+
+
+
+
+
+
+function calcular() {
+
+}
+
+function limpar() {
+
+}
+
+function exibir(res) {
+    document.querySelector(".resutado").innerHTML += res;
+}
