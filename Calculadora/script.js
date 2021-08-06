f08734246bf44da07bd89a470de1c2b067abef1a
Isaque Claudino <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2021-08-06 15:39:05 -0300

    Add files via upload
    calcula porcentagem(%) a troca de operadores (+/-) apaga um em um (<x| ) e calcula números Inteiros e decimais.

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -3,22 +3,32 @@ let ope = "";
 let v2 = "";
 let res = "";
 let numeroInt = true;
+let loopOpe = true;
 let Valormax = 8;
 
 function pegarValor(v) {
     if (v1.length < Valormax) {
-        if (v === Number(v) || v === ".") {
+        if (v === Number(v) || v === "." && v !== "+/-") {
             v1 += v;
+            
         } else {
-            if (v !== "+/-") {
+            if (v !== "+/-" && v1 !== "") { 
                 ope = v;
+                Valormax = v1.length;
             }
-            Valormax = v1.length;
         }
     } else {
-        if (v === Number(v) || v === ".") {
+        if (v === Number(v) || v === "." && v !== "+/-") {
             v2 += v;
         }
+    }
+
+    if (v === "+/-" && loopOpe) {
+        v1 = "-" + v1;
+        loopOpe = false;
+    } else if (v === "+/-" && loopOpe === false) {
+        v1 = v1.substr(1, v1.length);
+        loopOpe = true;
     }
 
 
@@ -28,6 +38,7 @@ function pegarValor(v) {
         numeroInt = false;
     }
 
+    
 
     exibir(v1 + ope + v2);
 }
@@ -54,6 +65,14 @@ function verificaOperador(operador) {
         case "/":
             res = v1 / v2;
             break;
+        case "%":
+            if (isNaN(v2)) {
+                res = v1 / 100;
+            } else {
+                v1 = v1 / 100;
+                v2 = v2 / 100;
+                res = v1 + v2;
+            }
     }
 }
 
@@ -63,9 +82,23 @@ function calcular() {
     reset();
 }
 
-function limpar() {
+function limparAll() {
     reset();
     exibir("CLEAR");
+}
+
+function limpar() {
+    if (v1 !== "" && v2 !== "") {
+        v2 = v2.substr(0, v2.length - 1);
+    } else if (v2 === "") {
+        if (ope !== "") {
+            ope = "";
+        } else {
+            v1 = v1.substr(0, v1.length - 1);
+        }
+    }
+
+    exibir(v1 + ope + v2);
 }
 
 function exibir(res) {
